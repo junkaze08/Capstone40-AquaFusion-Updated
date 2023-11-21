767d07af49c10a3c19d04e8743d56d0a31d2bb78
junkaze08 <b2xjunester@gmail.com>
2023-11-21 05:16:40 +0000

    Standardized Reflection of Data

--- sensor_DHT22.py
+++ sensor_DHT22.py
@@ -19,11 +19,20 @@ firebase_admin.initialize_app(cred_firestore, {
     'projectId': FIREBASE_CONFIG['projectId'],
 }, name='firestore')
 
-realtime_db = db.reference('/dht22_temperature_humidity', app=firebase_admin.get_app(name='realtime'))
+realtime_db_humidity = db.reference('/dht22_temperature_humidity', app=firebase_admin.get_app(name='realtime'))
+
+realtime_db_temperature = db.reference('/dht22_temperature_temperature', app=firebase_admin.get_app(name='realtime'))
 
 db = firestore.client(app=firebase_admin.get_app(name='firestore'))
 
-last_firestore_upload_time = time.time()
+# Reference to the Firestore collection for humidity
+humidity_collection = db.collection('DHT22_Humidity')
+
+# Reference to the Firestore collection for temperature
+temperature_collection = db.collection('DHT22_Temperature')
+
+# Time tracking variables
+last_firestore_upload_time = 0
 
 while True:
     try:
@@ -31,46 +40,54 @@ while True:
         if humidity is not None and temperature is not None:
             temperature = round(temperature, 1)
             humidity = round(humidity, 1)
-            
+
             print("Temperature: {:.1f}C, Humidity: {:.1f}%".format(temperature, humidity))
 
             if temperature < 35:
-                message = "Ambient Temperature"
+                temperature_status = "Ambient Temperature"
             elif temperature <= 40:
-                message = "Warning Exceeding Ambient Temperature"
+                temperature_status = "Warning Exceeding Ambient Temperature"
             elif temperature <= 50:
-                message = "Exceeding Maximum Temperature"
+                temperature_status = "Exceeding Maximum Temperature"
             else:
-                message = "Temperature within a safe range"
+                temperature_status = "Temperature within a safe range"
 
-            print(message)
+            print(temperature_status)
 
-            # Create a dictionary with the data to send to Firebase
-            data_realtime_db = {
-                "temperature": temperature,
-                "humidity": humidity,
-                "status": message
+            # Single dictionary for Realtime Database
+            data_realtime_db_humidity = {
+                "humidity": humidity
             }
             
-            # Send the data to Firebase Realtime Database
-            realtime_db.set(data_realtime_db)
+            data_realtime_db_temperature = {
+                "temperature": temperature,
+                "status": temperature_status
+            }
+
+            # Separate dictionaries for Firestore
+            data_firestore_temperature = {
+                "temperature": temperature,
+                "status": temperature_status
+            }
+
+            data_firestore_humidity = {
+                "humidity": humidity
+            }
+
+            # Send data to Realtime Database
+            realtime_db_humidity.set(data_realtime_db_humidity)
+            realtime_db_temperature.set(data_realtime_db_temperature)
 
             # Get the current time
             current_time = time.time()
-            
-            if (current_time - last_firestore_upload_time) >= 60:  # Check if 60 seconds have passed
+
+            if (current_time - last_firestore_upload_time) >= 5:  # Check if 60 seconds have passed
                 last_firestore_upload_time = current_time
 
-                # Dictionary with the data to send to Firestore
-                data_firestore = {
-                    "temperature": temperature,
-                    "humidity": humidity,
-                    "status": message
-                    # Add more data as needed
-                }
+                # Send data to Firestore collections
+                doc_ref_temperature = temperature_collection.add(data_firestore_temperature)
+                doc_ref_humidity = humidity_collection.add(data_firestore_humidity)
 
-                doc_ref = db.collection('dht22_temperature_humidity').add(data_firestore)
-            
         else:
             print("Sensor failure. Check wiring.")
 
