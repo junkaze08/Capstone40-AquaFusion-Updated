fcf0ead57a9995a72817b9c6a56b4611180c7dbc
junkaze08 <b2xjunester@gmail.com>
2023-11-29 17:24:34 +0000

    Allocating Unique Subprocess configuration

--- main.py
+++ main.py
@@ -2,44 +2,50 @@
 import subprocess
 import signal
 import time
+import os
 
-y = 0.1
+y = 5
 
 python3_path = '/usr/bin/python3'
 
-process1 = subprocess.Popen([python3_path, 'ultra_plant.py'])
+time.sleep(y)
+
+process2 = subprocess.Popen([python3_path, 'ultrasonic_plant.py'])
 
 time.sleep(y)
 
-process2 = subprocess.Popen([python3_path, 'ultra_water.py'])
+process3 = subprocess.Popen([python3_path, 'ultrasonic_water.py'])
 
 time.sleep(y)
 
-process2 = subprocess.Popen([python3_path, 'sensor_analogTDS.py'])
+process4 = subprocess.Popen([python3_path, 'sensor_analogTDS.py'])
 
 time.sleep(y)
 
-process2 = subprocess.Popen([python3_path, 'sensor_DHT22.py'])
+process5 = subprocess.Popen([python3_path, 'sensor_DHT22.py'])
 
 time.sleep(y)
 
-process2 = subprocess.Popen([python3_path, 'sensor_ds18b20.py'])
+process6 = subprocess.Popen([python3_path, 'sensor_ds18b20.py'])
 
 time.sleep(y)
 
-process2 = subprocess.Popen([python3_path, 'sensor_PH_Level.py'])
+process7 = subprocess.Popen([python3_path, 'sensor_PH_Level.py'])
 
 time.sleep(y)
-
-process2 = subprocess.Popen([python3_path, 'sensor_checker.py'])
 
 try:
     while True:
-        time.sleep(1)
+        time.sleep(10)
+        os.system('clear')
 except KeyboardInterrupt:
     print("Ctrl+C detected. Stopping processes...")
-    process1.send_signal(signal.SIGINT)
     process2.send_signal(signal.SIGINT)
+    process3.send_signal(signal.SIGINT)
+    process4.send_signal(signal.SIGINT)
+    process5.send_signal(signal.SIGINT)
+    process6.send_signal(signal.SIGINT)
+    process7.send_signal(signal.SIGINT)
 
 #Eradicating __pycache__    
 #find . | grep -E "__pycache__" | xargs rm -rf
