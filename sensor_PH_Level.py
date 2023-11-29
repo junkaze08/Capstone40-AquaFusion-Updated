2f0ab38a27f80b41d3c45d6b9017c806eef9bcb5
junkaze08 <b2xjunester@gmail.com>
2023-11-29 21:03:16 +0000

    Updated Calibration Code + Median Statistical Analysis

--- sensor_PH_Level.py
+++ sensor_PH_Level.py
@@ -1,10 +1,8 @@
 # AquaFusion - BSIT 4A - CPSTONE
+import Adafruit_ADS1x15
 import time
-import pyrebase
-import busio
-import board
-import adafruit_ads1x15.ads1115 as ADS
-from adafruit_ads1x15.analog_in import AnalogIn
+from collections import deque
+import statistics
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db, firestore
@@ -30,60 +28,75 @@ db = firestore.client(app=firebase_admin.get_app(name='firestore'))
 
 unique_Id = WORKGROUP_ID['uniqueId']
 
-# Initialize the I2C bus and ADS1115 ADC
-i2c = busio.I2C(board.SCL, board.SDA)
-ads = ADS.ADS1115(i2c)
+adc = Adafruit_ADS1x15.ADS1115()
 
-# Create an analog input channel on the ADS1115 (A0 in this example)
-chan = AnalogIn(ads, ADS.P0)
+RESOLUTION = 65535  # 16-bit
+GAIN = 1
 
-# pH calibration coefficients (replace with your calibration data)
-m = 1.0  # Replace with your calibration coefficient
-b = 0.0  # Replace with your calibration coefficient
+b = 0.00
+m = 0.167
 
 # Create a variable to track the last time data was sent to Firestore
 last_firestore_upload_time = time.time()
 
-# Function to convert raw ADC value to pH
-def convert_to_ph(raw_value):
-    # Divide raw value by 1000 to get a decimal value
-    voltage = raw_value / 1000  
+# Number of measurements for calculating mean and standard deviation
+MEASUREMENT_COUNT = 10
 
-    # Apply calibration equation
-    pH_val = m * voltage + b
-    
-    # Round to 1 decimal place
-    pH = round(pH_val, 1)
+# Number of recent pH values to consider in the rolling median filter
+ROLLING_MEDIAN_WINDOW = 5
 
-    return pH
+# Lists to store pH values for statistical analysis
+pH_values = []
+rolling_pH_values = deque(maxlen=ROLLING_MEDIAN_WINDOW)
 
 try:
 # Read the pH level and send data to both Realtime Database and Firestore
     while True:
+        measurings = 0
         utc_offset = 8
 
         curr_time = time.gmtime(time.time() + utc_offset * 3600)
         form_time = time.strftime("%H:%M:%S", curr_time)
-        # Read the raw analog value
-        raw_value = chan.value
-
-        # Convert the raw value to pH
-        pH = convert_to_ph(raw_value)
         
-        if pH >= 0:
+        for i in range(MEASUREMENT_COUNT):
+            # Read ADC channel 1 (A1)
+            measurings += adc.read_adc(1, gain=GAIN)
+            time.sleep(0.01)
+
+        # Calculate voltage
+        voltage = (6.144 / RESOLUTION) * (measurings / MEASUREMENT_COUNT)
+
+        # Calculate pH
+        pH_value = 7 + ((2.5 - voltage) / m) + b
+        finalpH = round(pH_value - 4.05, 2)
+
+        # Store the pH value in the list
+        pH_values.append(finalpH)
+
+        # Keep only the last MEASUREMENT_COUNT values for statistical analysis
+        pH_values = pH_values[-MEASUREMENT_COUNT:]
+
+        # Calculate rolling median
+        rolling_pH_values.append(finalpH)
+        median_pH = statistics.median(rolling_pH_values)
+
+        # Ignore values that deviate significantly from the rolling median
+        if abs(finalpH - median_pH) < 0.5:  # Adjust the threshold as needed
+            print(f"pH: {finalpH:.2f}")
+            print(form_time)
+        else:
+            print(f"Ignoring outlier pH value: {finalpH:.2f}")
+        
+        if finalpH >= 0:
             checkStatus = True
-
-        # Print the pH level
-        print(f'pH Level: {pH}')
-        print(form_time)
-        
+            
         # Get the current time
         current_time = time.time()
 
         # Send data to Realtime Database every 1 second
         data_realtime_db = {
             "Status": checkStatus,
-            "ph_level": pH
+            "ph_level": finalpH
         }
         realtime_db.update(data_realtime_db)
 
@@ -91,14 +104,13 @@ try:
         if (current_time - last_firestore_upload_time) >= 5:  # 60 seconds
             last_firestore_upload_time = current_time
             data_firestore = {
-                "ph_level": pH,
+                "ph_level": finalpH,
                 "timestamp": form_time,
                 "workgroupId": unique_Id            
             }
             doc_ref = db.collection('PH_values').add(data_firestore)
-        
-        # Adjust the delay based on your needs (e.g., how often you want to read the pH value)
-        time.sleep(5)
+
+        time.sleep(4)
 
 except KeyboardInterrupt:
     checkStatus = False
