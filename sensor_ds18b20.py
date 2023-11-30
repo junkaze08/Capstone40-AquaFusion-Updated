76c89d7382594fd2ace5bbc068af5232273ef06d
junkaze08 <b2xjunester@gmail.com>
2023-11-30 12:18:12 +0000

    Abstraction of Collections and Threshold Referencing Realtime Database

--- sensor_ds18b20.py
+++ sensor_ds18b20.py
@@ -6,7 +6,7 @@ import busio
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db, firestore
-from config import FIREBASE_CONFIG, WORKGROUP_ID
+from config import FIREBASE_CONFIG, WORKGROUP_ID, FIREBASE_DS18B20
 
 # Initialize Firebase Admin for Realtime Database
 cred = credentials.Certificate(FIREBASE_CONFIG['serviceAccountKeyPath'])
@@ -21,7 +21,19 @@ firebase_admin.initialize_app(cred_firestore, {
 }, name='firestore')
 
 # Initialize Realtime Database
-realtime_db = db.reference('/DS18B20_water_temperature', app=firebase_admin.get_app(name='realtime'))
+realtime_db = db.reference(FIREBASE_DS18B20['sensorCollection'], app=firebase_admin.get_app(name='realtime'))
+
+realtime_db_threshold_lower = db.reference(FIREBASE_DS18B20['sensorLower'], app=firebase_admin.get_app(name='realtime'))
+
+realtime_db_threshold_upper = db.reference(FIREBASE_DS18B20['sensorUpper'], app=firebase_admin.get_app(name='realtime'))
+
+realtime_db_threshold_normal = db.reference(FIREBASE_DS18B20['sensorNormal'], app=firebase_admin.get_app(name='realtime'))
+
+temperature_normal = realtime_db_threshold_normal.get()
+
+temperature_limits_lower = realtime_db_threshold_lower.get()
+
+temperature_limits_upper = realtime_db_threshold_upper.get()
 
 # Initialize Firestore
 db = firestore.client(app=firebase_admin.get_app(name='firestore'))
@@ -53,6 +65,25 @@ def read_temp():
         temp_c = float(temp_string) / 1000.0
         return temp_c
 
+def on_threshold_change_lower(event):
+    global temperature_limits_lower
+    if event.data is not None:
+        temperature_limits_lower = str(event.data)
+        print("Threshold lower levels updated:", temperature_limits_lower)
+    else:
+        print("Threshold levels data not available in the database.")
+    
+def on_threshold_change_upper(event):
+    global temperature_limits_upper
+    if event.data is not None:
+        temperature_limits_upper = str(event.data)
+        print("Threshold upper levels updated:", temperature_limits_upper)
+    else:
+        print("Threshold levels data not available in the database.")
+
+threshold_listener_lower = realtime_db_threshold_lower.listen(on_threshold_change_lower)
+threshold_listener_upper = realtime_db_threshold_upper.listen(on_threshold_change_upper)
+
 try:
     while True:
         utc_offset = 8
@@ -67,16 +98,25 @@ try:
         if temperature is not None:
             print(f"Temperature: {temperature}°C")
             print(form_time)
-
+            
             if temperature >= 0:
                 checkStatus = True
 
-            if temperature < 18:
-                temperature_status = "Warning: Cold Temperature"
-            elif 18 <= temperature <= 26:
-                temperature_status = "Optimal Temperature"
-            else:
-                temperature_status = "Warning: Hot Temperature"
+            lower_key = FIREBASE_DS18B20['sensorConditionalLower']
+            upper_key = FIREBASE_DS18B20['sensorConditionalUpper']
+
+            if temperature_normal and lower_key in temperature_normal and upper_key in temperature_normal:
+                lower_limit = temperature_normal[lower_key]
+                upper_limit = temperature_normal[upper_key]
+                
+                if temperature < lower_limit:
+                    temperature_status = "Warning: Cold Temperature"
+                elif lower_limit <= temperature <= upper_limit:
+                    temperature_status = "Optimal Temperature"
+                else:
+                    temperature_status = "Warning: Hot Temperature"
+                    
+            print(temperature_status)
             
             if (current_time - last_realtime_upload_time) >= 1:  # Upload to Realtime Database every 5 seconds
                 
@@ -105,6 +145,8 @@ try:
         time.sleep(5)
 
 except KeyboardInterrupt:
+    threshold_listener_lower.close()
+    threshold_listener_upper.close()
     checkStatus = False
     data_realtime_db = {
         "Status": checkStatus,    
